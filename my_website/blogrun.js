7a04ab3eda2bafe2a30e5ec3d777ea4b4a930e44
MizarZh <2267243023@qq.com>
2020-04-01 23:29:48 +0800

    add border-radius transition

---
+++ my_website/blogrun.js
@@ -0,0 +1,11 @@
+function page(){
+    let x = document.getElementsByClassName('left-bottom');
+    x.addEventListener('scroll',function(){
+        if(x.clientY < 10){
+            var a = function(){
+                
+            }
+        }
+    })
+}
+
